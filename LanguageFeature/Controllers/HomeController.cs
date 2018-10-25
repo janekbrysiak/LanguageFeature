0749966396a20629245a8c0bbd0d57357426d597
jan.brysiak <jan.brysiak@wp.pl>
2018-10-25 09:49:55 +0200

    Dodaj pliki projektów.

---
+++ LanguageFeature/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LanguageFeature.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LanguageFeature.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            ShoppingCart cart = new ShoppingCart { Products = Product.GetProducts() };
+            Product[] productsArray = {
+                new Product { Name = "Kajak", Price = 275M },
+                new Product{Name="Kamizelka ratunkowa",Price=48.95M},
+                new Product {Name="Flaga narożna",Price=34.95M },
+                new Product  {Name="Piłka nożna",Price=19.50M}
+            };
+            decimal cartTotal = cart.TotalPrices();
+            decimal arrayTotal = productsArray.FilterByPrice(20).TotalPrices();
+
+            return View("Index", new string[]
+            {
+                $"Razem koszyk:{cartTotal:C2}",
+                $"Razem tablica:{arrayTotal:C2}"
+            });
+        }
+
+    }
+}
---
+++ LanguageFeature/Models/Product.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LanguageFeature.Models
+{
+    public class Product
+    {
+        public Product(bool stock=true)
+        {
+            InStock = stock;
+        }
+        public string Name { get; set; }
+        public decimal?  Price { get; set; }
+        public Product Related { get; set; }
+        public string Category { get; set; } = "Wodniarstwo";
+        public bool InStock { get; } = true;
+
+        public static Product[] GetProducts()
+        {
+            Product kajak = new Product
+            {
+                Name = "Kajak",
+                Price = 275,
+                Category="Sporty wodne"
+            };
+            Product lifejacket = new Product(false)
+            {
+                Name = "Kamizelka ratunkowa",
+                Price = 48.95M
+            };
+            kajak.Related = lifejacket;
+            return new Product[] { kajak, lifejacket, null };    
+
+        }
+
+    }
+}
